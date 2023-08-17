d489808ce869bb718a1ba1c61d115474abfef110
Rohith Gadde <50209060+RohithGadde@users.noreply.github.com>
2023-08-17 10:46:13 -0400

    Update game.py
    Spell Check.

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ def position_choice():
     choice = 'wrong'
 
     while choice not in ['0', '1', '2']:
-        choice = input("Please pick a positon (0,1,2):")
+        choice = input("Please pick a position (0,1,2):")
 
         if choice not in ['0', '1', '2']:
             print("Wrong Input!")
